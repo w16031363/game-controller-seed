27130e2440af8540423e30e2a5d9b6846da5f8d2
w16031363 <mattstorr1@sky.com>
2018-05-13 21:30:19 +0100

    final push

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <EthernetInterface.h>
 #include <rtos.h>
 #include <mbed_events.h>
-
+#include <udp.h>
 #include <FXOS8700Q.h>
 #include <C12832.h>
 
@@ -13,97 +13,160 @@ C12832 lcd(D11, D13, D12, D7, D10);
 /* event queue and thread support */
 Thread dispatch;
 EventQueue periodic;
-/* Accelerometer */
-I2C i2c(PTE25, PTE24);
-FXOS8700QAccelerometer acc(i2c, FXOS8700CQ_SLAVE_ADDR1);
-/*YOU will have to hardwire the IP address in here */
-SocketAddress lander("192.168.1.165",65200);
-SocketAddress dash("192.168.1.13",65250);
-EthernetInterface eth;
-UDPSocket udp;
+
+/*LED outputs*/
+DigitalOut red(LED_RED);
+DigitalOut green(LED_GREEN);
+void greenOn(void) { green.write(0); }
+void greenOff(void) { green.write(1); }
+void redOn(void) { red.write(0); }
+void redOff(void) { red.write(1); }
+
 /* Input from Potentiometers */
 AnalogIn  left(A0);
+AnalogIn right(A1);
 
-float constrain(float value, float lower, float upper) {
-    if( value<lower ) value=lower;
-    if( value>upper ) value=upper;
-    return value;
-}
 /* User input states */
-/* define variables to hold the users desired actions.*/
-float throttle = 0.0;
-float last1 = 0;
-float last2 = 0;
-float integral = 0;
-/* Task for polling sensors */
-void user_input(void){
-    char buffer[512];
-    motion_data_units_t a;
-    acc.getAxis(a);
-    lcd.locate(0,0);
-    float magnitude = sqrt(a.x*a.x+a.x*a.x+a.x*a.x);
-    lcd.printf("axis x:%3.1f (%3.1f)\n",a.x, magnitude);
-    a.x = a.x/magnitude;
-    a.y = a.y/magnitude;
-    a.y = a.y/magnitude;
-    lcd.printf("axis x:%3.1f (%3.1f)\n",a.x, magnitude);
-    float angle = asin(a.x);
-    lcd.printf("angle: %.2f ",angle);
-    float P = (error-last1);
-    float I = (error);
-    float D = (error-2*last1+last2);
-    float Kp =5, Ki = 3, Kd = 3;
-//    float Kp =10, Ki = 5, Kd = 0.1;
-    throttle += ( Kp*P + Ki*I  + Kd*D)/1000;
-    throttle = constrain(throttle,0,1);
-    sprintf(buffer,"throttle:%f\n",throttle);
-    send(buffer,strlen(buffer));
-    last2 = last1;
-    last1 = error;
-    sprintf(buffer,"roll:%.2f\nthrottle:%.2f\n", angle, throttle);
-    udp.sendto( lander, buffer, strlen(buffer));
-    /*TODO decide on what roll rate -1..+1 to ask for */
+float throttle;
+float roll;
 
-    /*TODO decide on what throttle setting 0..100 to ask for */
-}
+/* YOU will have to hardwire the IP address in here */
+SocketAddress lander("192.168.1.217",65200);
+SocketAddress dash("192.168.1.217",65250);
+
+EthernetInterface eth;
+UDPSocket udp;
+
 /* States from Lander */
-/*TODO Variables to hold the state of the lander as returned to
-    the MBED board, including altitude,fuel,isflying,iscrashed
-*/
+char altitude [50];
+char fuel [50];
+char flying [50];
+char crashed [50];
+char orientation [50];
+char velocityX [50];
+char velocityY [50];
+
+int send(char *m, size_t s, SocketAddress dest)
+{
+  nsapi_size_or_error_t r = udp.sendto(dest, m, s);
+  return r;
+}
+int receive(char *m, size_t s, SocketAddress reply)
+{
+  nsapi_size_or_error_t r = udp.recvfrom(&reply, m, s);
+  return r;
+}
 
 /* Task for synchronous UDP communications with lander */
 void communications(void){
     SocketAddress source;
-    /*Create and format the message to send to the Lander */
-    char buffer[512];
-    sprintf(buffer, "command:!\n....");
-    /* Send and receive messages*/
-    udp.sendto( lander, buffer, strlen(buffer));
-    nsapi_size_or_error_t  n =
-                 udp.recvfrom(&source, buffer, sizeof(buffer));
-    buffer[n] = '\0';
-    /* Unpack incomming message */
-    /*split message into lines*/
-    /*for each line */
-    char *nextline, *line;
-    for(
-      line = strtok_r(buffer, "\r\n", &nextline);
-      line != NULL;
-      line = strtok_r(NULL, "\r\n", &nextline)
-    ){
-      /* split into key value pairs */
-    char *key, *value;
-    for(
-      key = strtok(line, ":");
-      value = strtok(NULL, ":");
-    )
-     /*convert value strings into state variables */
 
-      if( strcmp(key,"altitude")==0 ){
-        /*do something with the 'altitude' value*/
-      }
-   }
+    char throttleString [50];
+    itoa (throttle, throttleString, 10);
+
+    char rollString[50];
+    if(roll > 0)
+    {
+      sprintf(rollString, "+%.1lf" , roll);
+    }
+    else
+    {
+      sprintf(rollString, "%.1lf" , roll);
+    }
+
+    char buffer [512];
+    sprintf(buffer, "command:!\nthrottle:%s\nroll:%s\n", throttleString, rollString);
+    udp.sendto( lander, buffer, strlen(buffer));
+    printf("Message sent to Lander...\n");
+    printf("Waiting for response...\n");
+    nsapi_size_or_error_t n = udp.recvfrom(&source, buffer,sizeof(buffer));
+    printf("Message received!\n");
+    buffer[n] = '\0';
+    printf("%s\n", buffer);
+
+    char line1 [50];
+    char line2 [50];
+    char line3 [50];
+    char line4 [50];
+    char line5 [50];
+    char line6 [50];
+    char line7 [50];
+    char line8 [50];
+
+    char *nextLine;
+
+    nextLine = strtok(buffer, "\n");
+    strcpy(line1, nextLine);
+    printf("%s\n", line1);
+
+    nextLine = strtok(NULL, "\n");
+    strcpy(line2, nextLine);
+
+    printf("%s\n", line2);
+
+    nextLine = strtok(NULL, "\n");
+    strcpy(line3, nextLine);
+    printf("%s\n", line3);
+
+    nextLine = strtok(NULL, "\n");
+    strcpy(line4, nextLine);
+    printf("%s\n", line4);
+
+    nextLine = strtok(NULL, "\n");
+    strcpy(line5, nextLine);
+    printf("%s\n", line5);
+
+    nextLine = strtok(NULL, "\n");
+    strcpy(line6, nextLine);
+    printf("%s\n", line6);
+
+    nextLine = strtok(NULL, "\n");
+    strcpy(line7, nextLine);
+    printf("%s\n", line7);
+
+    nextLine = strtok(NULL, "\n");
+    strcpy(line8, nextLine);
+    printf("%s\n", line8);
+
+    buffer[0] = '\0';
+
+    nextLine = strtok(line2, ":");
+    nextLine = strtok(NULL, "\n");
+    strcpy(altitude, nextLine);
+
+    nextLine = strtok(line3, ":");
+    nextLine = strtok(NULL, "\n");
+    strcpy(fuel, nextLine);
+
+    nextLine = strtok(line4, ":");
+    nextLine = strtok(NULL, "\n");
+    strcpy(flying, nextLine);
+
+    nextLine = strtok(line5, ":");
+    nextLine = strtok(NULL, "\n");
+    strcpy(crashed, nextLine);
+
+    nextLine = strtok(line6, ":");
+    nextLine = strtok(NULL, "\n");
+    strcpy(orientation, nextLine);
+
+    nextLine = strtok(line7, ":");
+    nextLine = strtok(NULL, "\n");
+    strcpy(velocityX, nextLine);
+
+    nextLine = strtok(line8, ":");
+    nextLine = strtok(NULL, "\n");
+    strcpy(velocityY, nextLine);
+
+    sprintf(buffer, "altitude:%s\nfuel:%s\nflying:%s\ncrashed:%s\norientation:%s\nVx:%s\nVy:%s\n"
+    , altitude, fuel, flying, crashed, orientation, velocityX, velocityY);
+
+    lcd.locate(0,0);
+    lcd.printf("fuel: %s\n", fuel);
+
+    send(buffer, strlen(buffer), dash);
 }
+
 /* Task for asynchronous UDP communications with dashboard */
 void dashboard(void){
     /*TODO create and format a message to the Dashboard */
@@ -112,31 +175,46 @@ void dashboard(void){
     */
 }
 
+/* Task for polling sensors */
+void user_input(void){
+    roll = (right.read() * 2) - 1;
+    throttle = (int)round(left.read() * 100);
+    lcd.locate(0,0);
+    lcd.printf("\nThrottle: %5.2f roll:%5.2f", throttle, roll);
+    if (throttle >= 90)
+    {
+      redOn();
+      greenOff();
+      lcd.printf("\nWatch your fuel!!");
+    }
+    else
+    {
+      greenOn();
+      redOff();
+      lcd.printf("\n                   ");
+    }
+    communications();
+}
+
 int main() {
-    acc.enable();
+    eth.connect();
     /* ethernet connection : usually takes a few seconds */
     printf("connecting \n");
-    eth.connect();
     /* write obtained IP address to serial monitor */
     const char *ip = eth.get_ip_address();
-    printf("IP address is: %s\n", ip ? ip : "No IP");
+    printf("IP Address is: %s\n", ip ? ip : "No IP" );
     /* open udp for communications on the ethernet */
-    udp.open( &eth);
+    udp.open(&eth);
+
     printf("lander is on %s/%d\n",lander.get_ip_address(),lander.get_port() );
     printf("dash   is on %s/%d\n",dash.get_ip_address(),dash.get_port() );
+
     /* periodic tasks */
-    /* call periodic tasks;
-            communications, user_input, dashboard
-            at desired rates.*/
-    periodic.call_every(50, user_input);
+    periodic.call_every(0.5, user_input);
+
     /* start event dispatching thread */
     dispatch.start( callback(&periodic, &EventQueue::dispatch_forever) );
-    while(1) {
-        /* update display at whatever rate is possible */
-        /*TODO show user information on the LCD */
-        /*TODO set LEDs as appropriate to show boolean states */
 
-        wait(1);/*TODO you may want to change this time
-                    to get a responsive display */
-    }
+    while(1) {}
+
 }
